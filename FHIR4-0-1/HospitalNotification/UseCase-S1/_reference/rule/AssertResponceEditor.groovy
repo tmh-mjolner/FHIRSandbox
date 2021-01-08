0ec11e3ae6a37783e2c26d5f28e384128943d2e6
MedComAnders <69896426+MedComAnders@users.noreply.github.com>
2021-01-08 15:04:35 +0100

    Not used exampels deletet

--- FHIR4-0-1/HospitalNotification/UseCase-S1/_reference/rule/AssertResponceEditor.groovy
+++
@@ -1,7 +0,0 @@
-/*
- rule.summary=try to chage the response body
- rule.description= the responce body has been changed
-*/
-response.body= "VIoler er rode biler er sorte"
-
-assert response.body: "VIoler er rode biler er sorte"
