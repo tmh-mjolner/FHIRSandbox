af256c2811bc02cbd53bfb4a235645f643248df1
MedComAnders <69896426+MedComAnders@users.noreply.github.com>
2021-01-09 15:03:01 +0100

    Exampels following US_H_V_1

---
+++ FHIR4-0-1/CareCommunication/Userstory/_reference/rule/AssertResponceEditor.groovy
@@ -0,0 +1,7 @@
+/*
+ rule.summary=try to chage the response body
+ rule.description= the responce body has been changed
+*/
+response.body= "VIoler er rode biler er sorte"
+
+assert response.body: "VIoler er rode biler er sorte"
