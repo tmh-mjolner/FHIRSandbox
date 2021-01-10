247c0032e924784ec462fa8ff086565d6d8cc765
MedComAnders <69896426+MedComAnders@users.noreply.github.com>
2021-01-10 12:52:53 +0100

    structure updated to fit TS.

--- FHIR4-0-1/HospitalNotification/Userstory/_reference/rule/AssertHeader.groovy
+++
@@ -1,8 +0,0 @@
-/*
- rule.summary=Response ETag header cannot be empty
- rule.description=Validates the 'ETag' header in the response
-*/
-
-
-
-assert response.header('ETag').isNotEmpty(): "Could not find 'ETag' header in response"
